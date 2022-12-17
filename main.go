e8f1aab616c04c4762a9af5d7178c22e8fed6dbe
Ezaz Ahmed <ezaz.dev@gmail.com>
2022-12-17 12:10:01 +0600

    Add: go.mod

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello! From Go World</h1>")
+	fmt.Fprint(w, "<h1>Hello! Tonmoy</h1>")
 }
 
 func main() {
